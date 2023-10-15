e5366fbb2727f1932b731f5e641a27ab42694fa2
0xcacti <0xcacti@gmail.com>
2023-10-14 21:39:48 -0400

    scaffold the crap out of this thing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::process;
+
+use clap::{crate_version, Parser, Subcommand};
+use proposal::proposal::Config;
+
+/// proposal is a command line tool for getting a epoch proposal information for Ethereum
+/// validators
+#[derive(Debug, Parser)]
+#[command(name="proposal", version=crate_version!(), about="proposal info", long_about = "Get validator proposal information for an epoch", arg_required_else_help(true))]
+struct AppParser {
+    /// The subcommand to run
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Get a list of validators that will be proposers for the given epoch
+    Get,
+    /// Check if the given validators will be proposers for the given epoch
+    Check {
+        /// The list of validators in question
+        #[arg(required = true)]
+        validators: Vec<String>,
+    },
+}
 fn main() {
-    println!("Hello, world!");
+    let mut config = Config::from(Config::figment()).unwrap();
+    let app = AppParser::parse();
+    match app.command {
+        Some(Commands::Get) => {
+            println!("Get");
+        }
+        Some(Commands::Check { validators }) => {
+            println!("Check: {:?}", validators);
+        }
+        None => {
+            eprintln!("No command provided");
+            process::exit(1);
+        }
+    }
 }
